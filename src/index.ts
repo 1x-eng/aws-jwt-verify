c0ea055b55d9e76be517a179a54f85135e22ec2d
Pruthvi Kumar <pruthvi.kumar@seermedical.com>
2023-10-03 19:05:48 +1100

    refactor: add exports required for explicit jwkscache to avoid problem with importing submodules

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@
 
 export { JwtRsaVerifier } from "./jwt-rsa.js";
 export { CognitoJwtVerifier } from "./cognito-verifier.js";
+
+// including these exports in index.ts to avoid typescript from complaining re. inability to import from submodules, which was the problem while using official aws-jwt-verify package.
+export { SimpleJsonFetcher } from "./https.js";
+export { SimpleJwksCache } from "./jwk.js";
+export { CognitoAccessTokenPayload, CognitoIdTokenPayload, CognitoJwtPayload } from "./jwt-model.js";
